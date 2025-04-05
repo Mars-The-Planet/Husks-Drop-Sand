1fb0009ca123c6107a2d14088e43f48b6c871627
Matej <ruzicka.maty@gmail.com>
2025-04-05 16:49:16 +0200

    backported to 1.18.2

--- forge/src/main/java/com/mars/huskssand/HuskLootModifier.java
+++ forge/src/main/java/com/mars/huskssand/HuskLootModifier.java
@@ -1,43 +1,46 @@
 package com.mars.huskssand;
 
+import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.levelgen.RandomSource;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Random;
 
 public class HuskLootModifier extends LootModifier {
-    private static final Item sandDrop = BuiltInRegistries.ITEM.get(new ResourceLocation("sand"));
-    public static final MapCodec<HuskLootModifier> CODEC = RecordCodecBuilder.mapCodec(inst ->
-            LootModifier.codecStart(inst).apply(inst, HuskLootModifier::new));
+    private static final Item sandDrop = Registry.ITEM.get(new ResourceLocation("sand"));
 
     public HuskLootModifier(LootItemCondition[] conditionsIn) {
         super(conditionsIn);
     }
 
+    @NotNull
     @Override
-    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext lootContext) {
+    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext lootContext) {
         for(LootItemCondition condition : this.conditions) {
             if(!condition.test(lootContext)) {
                 return generatedLoot;
             }
         }
 
-        RandomSource random = lootContext.getRandom();
-        int rolls = random.nextIntBetweenInclusive(HusksSandConfig.min_rolls, HusksSandConfig.max_rolls);
+        Random random = lootContext.getRandom();
+        int rolls = random.nextInt(HusksSandConfig.min_rolls, HusksSandConfig.max_rolls);
         Player player = lootContext.getParamOrNull(LootContextParams.LAST_DAMAGE_PLAYER);
 
         if(player != null) {
@@ -45,7 +48,7 @@ public class HuskLootModifier extends LootModifier {
             int lootingLevel = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.MOB_LOOTING, weapon);
 
             if (lootingLevel > 0)
-                rolls = rolls + random.nextIntBetweenInclusive(0, lootingLevel);
+                rolls = rolls + random.nextInt(0, lootingLevel);
         }
 
         generatedLoot.add(new ItemStack(sandDrop, rolls));
@@ -53,8 +56,16 @@ public class HuskLootModifier extends LootModifier {
         return generatedLoot;
     }
 
-    @Override
-    public Codec<? extends IGlobalLootModifier> codec() {
-        return CODEC.codec();
+    public static class Serializer extends GlobalLootModifierSerializer<HuskLootModifier> {
+        @Override
+        public HuskLootModifier read(ResourceLocation location, JsonObject object,
+                                     LootItemCondition[] ailootcondition) {
+            return new HuskLootModifier(ailootcondition);
+        }
+
+        @Override
+        public JsonObject write(HuskLootModifier instance) {
+            return new JsonObject();
+        }
     }
 }
--- forge/src/main/java/com/mars/huskssand/HusksSand.java
+++ forge/src/main/java/com/mars/huskssand/HusksSand.java
@@ -1,6 +1,7 @@
 package com.mars.huskssand;
 
 import com.mojang.serialization.Codec;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -13,15 +14,13 @@ import static com.mars.huskssand.Constants.MOD_ID;
 
 @Mod(MOD_ID)
 public class HusksSand {
-    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> GLOBAL_LOOT_MODIFIER_SERIALIZERS =
-            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, MOD_ID);
-
-    public static final RegistryObject<Codec<? extends HuskLootModifier>> MY_LOOT_MODIFIER =
-            GLOBAL_LOOT_MODIFIER_SERIALIZERS.register("husklootmodifier", HuskLootModifier.CODEC::codec);
+    public static final DeferredRegister<GlobalLootModifierSerializer<?>> lootModifiers = DeferredRegister.create(ForgeRegistries.Keys.LOOT_MODIFIER_SERIALIZERS, MOD_ID);
     public HusksSand() {
         CommonClass.init();
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        GLOBAL_LOOT_MODIFIER_SERIALIZERS.register(modEventBus);
+        lootModifiers.register(modEventBus);
+
+        lootModifiers.register("husklootmodifier", HuskLootModifier.Serializer::new);
     }
 }
